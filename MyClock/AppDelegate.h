de5a482aa1c260c6c4013b6b4a880a759afaeabf
AS <sjp785867364@gmail.com>
2015-12-07 16:25:46 +0800

    Initial Commit

---
+++ MyClock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyClock
+//
+//  Created by su on 15/12/7.
+//  Copyright (c) 2015年 YuBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyClock/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyClock
+//
+//  Created by su on 15/12/7.
+//  Copyright (c) 2015年 YuBo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
